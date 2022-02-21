ee91924f0be9808d554543ae1cc83641190b88f0
ThisIsAlperen <aydin.m.alperen@gmail.com>
2022-02-21 23:28:20 +0300

    Scroll main
    content is now scrollable, scroll with click will be added

---
+++ index.js
@@ -0,0 +1,11 @@
+const homePage = document.getElementById('homePage')
+const aboutPage = document.getElementById('aboutPage')
+const portfolioPage = document.getElementById('portfolioPage')
+const contactPage = document.getElementById('contactPage')
+
+const Scroll = document.getElementById('scroll')
+myFunction()
+
+function myFunction(){
+    Scroll.scrollIntoView()
+}
