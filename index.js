86d2743f3c34ab6f8f51df502d4dc8a736ccc6e2
ThisIsAlperen <aydin.m.alperen@gmail.com>
2022-02-22 23:07:09 +0300

    Re-writing
    There will be a job name that constantly changing between web developer and freelancer

--- index.js
+++ index.js
@@ -4,64 +4,108 @@ const portfolioPage = document.getElementById('portfolioPage')
 const contactPage = document.getElementById('contactPage')
 
 const Scroll = document.getElementById('scroll')
+const ScrollLinks = document.getElementById('scrollLinks')
+const Main = document.querySelector('main')
 
+document.getElementById('homeLink').classList.add('hover')
+
+NodeList.prototype.forEach = HTMLCollection.prototype.forEach = Array.prototype.forEach;
 
 myFunction()
 
 function myFunction() {
     Scroll.scrollIntoView()
 }
-b = document.getElementById('homeLink')
-document.getElementById('homeLink').onclick= function(e){
-    b.offsetTop
- console.log(b.offsetTop)
-    $('html,main').animate({ //animate element that has scroll
-        scrollTop: b.offsetTop //for scrolling
-    }, 1000);
-    e.preventDefault()
-   
-}
 
-a = document.getElementById('aboutLink')
-document.getElementById('aboutLink').onclick= function(e){
-    console.log(a.offsetTop)
-    console.log(a)
-    a.offsetTop
+
+ScrollLinks.onclick = function (e) {
+    var x = e.target.getAttribute('href')
+    var y = document.getElementById(x)
+
     $('html,main').animate({ //animate element that has scroll
-        scrollTop: a.offsetTop //for scrolling
+        scrollTop: y.offsetTop //for scrolling
+
     }, 1000);
     e.preventDefault()
 }
-    
+Main.addEventListener("scroll", function () {
+    ScrollLinks.children.forEach(child => {
+        child.children[0].classList = ''
+    });
+
+    if (Main.scrollTop < aboutPage.offsetTop - 200) {
+
+        document.getElementById('homeLink').classList.add('hover')
+    }
+    if (Main.scrollTop > aboutPage.offsetTop - 200 && Main.scrollTop < portfolioPage.offsetTop - 200) {
 
 
+        document.getElementById('aboutLink').classList.add('hover')
 
-// $(document).ready(function(){
-//     // Add smooth scrolling to all links
-//     $("a").on('click', function(event) {
-  
-//       // Make sure this.hash has a value before overriding default behavior
-//       console.log(this)
-    
-     
-//       if (this.hash !== "") {
-//         // Prevent default anchor click behavior
-//         event.preventDefault();
-  
-//         // Store hash
-//         var hash = this.hash;
-//         console.log(this.hash)
-        
-//         // Using jQuery's animate() method to add smooth page scroll
-//         // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-        
-//         $('html, main').animate({
-//           scrollTop: $(hash).offset().top
-//         }, 800, function(){
-     
-//           // Add hash (#) to URL when done scrolling (default click behavior)
-          
-//         });
-//       } // End if
-//     });
-//   });
+    }
+    if (Main.scrollTop > portfolioPage.offsetTop - 200 && Main.scrollTop < contactPage.offsetTop - 200) {
+        document.getElementById('portfolioLink').classList.add('hover')
+    }
+    if (Main.scrollTop > contactPage.offsetTop - 200) {
+        document.getElementById('contactLink').classList.add('hover')
+    }
+
+
+});
+
+setInterval(function () {
+
+    var k = ['Web Developer', 'Freelancer']
+    var x = document.getElementById('homeJob').children[0]
+    if (k[0] === x.innerText) {
+        k = k[1].split('')
+        setTimeout(function () { yyy(k); }, 750)
+    } 
+    if (k[1] === x.innerText){
+        k = k[0].split('')
+        setTimeout(function () { yyy(k); }, 750)
+    }
+    x = x.innerText.split('')
+    x = x.reverse()
+    console.log(x)
+    setTimeout(function () { xxx(x); }, 0)
+}, 4000)
+
+
+function xxx(x) {
+    console.log(x)
+    x.splice(0, 1)
+    if (x.length > 0) {
+        var y = ''
+        x.reverse()
+        for (i = 0; i < x.length; i++) {
+            y = y + x[i]
+        }
+        setTimeout(function () {
+            if (x.length == 1) {
+                y = ''
+                document.getElementById('homeJob').children[0].innerText = y
+
+            }
+        }, 100)
+
+        document.getElementById('homeJob').children[0].innerText = y
+        x.reverse()
+        setTimeout(function () { xxx(x); }, 50)
+    }
+    if(x.length == 0){
+        return;
+    }
+
+}
+function yyy(k) {
+    console.log(k)
+    y = document.getElementById('homeJob').children[0].innerText
+    y = y + k[0]
+    console.log(y)
+    k.splice(0, 1)
+
+
+    setTimeout(function () { xxx(k); }, 50)
+
+}
