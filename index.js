5ecfaf4bee7484f0b27db5e8b51f14f0f44587a6
ThisIsAlperen <aydin.m.alperen@gmail.com>
2022-02-24 12:45:42 +0300

    Update on Portfolio

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const job = document.getElementById('homeJob').children[0]
 
 // this is for the changing job with css
 const job2 = document.getElementById('changeJob')
-
+const jobDetail = document.getElementById('jobDetail')
 const carousel = document.getElementById('portfolioLinks')
 const carouselLeft = document.getElementsByClassName('fa-angle-left')
 const carouselRight = document.getElementsByClassName('fa-angle-right')
@@ -20,20 +20,20 @@ document.getElementById('homeLink').classList.add('hover')
 
 NodeList.prototype.forEach = HTMLCollection.prototype.forEach = Array.prototype.forEach;
 
-function hide(x){
+function hide(x) {
     x.classList.add('hide')
 }
-function show(x){
+function show(x) {
     x.classList.remove('hide')
 }
 myFunction()
 
 function myFunction() {
     Scroll.scrollIntoView()
-   
+
 }
 
-homePage.children[1].onclick = function(e){
+homePage.children[1].onclick = function (e) {
 
     $('html,main').animate({ //animate element that has scroll
         scrollTop: 716 //for scrolling
@@ -77,7 +77,7 @@ Main.addEventListener("scroll", function () {
 // ********vvvv This Part is for the changing the job vvvv******//
 // ************* I found a smoother way with css **********//
 //setTimeout(changeJob,1000)
-function changeJob(){
+function changeJob() {
     var k = ['Web Developer', 'Freelancer']
 
     var x = job.innerText
@@ -139,18 +139,18 @@ function clearJob(x) {
 
 carouselLeft[0].onclick = function () {
     var x = carousel.offsetLeft
-   
+
     console.log(x)
     if (x < 0) {
         x = x + 570
         console.log(x)
-        
+
         carousel.style.left = x + 'px'
     }
 }
 carouselRight[0].onclick = function () {
     var x = carousel.offsetLeft
-  
+
     if (x >= -570) {
         x = x - 570
         carousel.style.left = x + 'px'
@@ -158,34 +158,65 @@ carouselRight[0].onclick = function () {
 }
 // This is the new part for the changing the job
 var i;
-setInterval(function(){
-  
-    if(i == undefined){
+setInterval(function () {
+
+    if (i == undefined) {
         i = 0
     }
     job2.style.width = '5px'
-    setTimeout(function(){
-        if(i == 0){
+    setTimeout(function () {
+        if (i == 0) {
             hide(job2.children[0].children[0])
             job2.style.width = '133px'
             show(job2.children[0].children[1])
         }
-        if(i == 1){
+        if (i == 1) {
             hide(job2.children[0].children[1])
             job2.style.width = '100px'
             show(job2.children[0].children[2])
         }
-        if(i == 2){
+        if (i == 2) {
             hide(job2.children[0].children[2])
             job2.style.width = '147px'
             show(job2.children[0].children[0])
         }
         i++
-    },800)
-    if(i>2){
+    }, 800)
+    if (i > 2) {
         i = 0
     }
-},2500)
+}, 2500)
 //****************************************************/
 
+carousel.onclick = function (e) {
+    portfolioPage.children[1].style.opacity = '0';
+    portfolioPage.children[2].style.opacity = '0';
+    console.log(e.target.getAttribute('href'))
+    setTimeout(function () {
+        hide( portfolioPage.children[1])
+        hide( portfolioPage.children[2])
+        show(jobDetail)
+       
+    }, 350)
+    setTimeout(function () {
+        
+        jobDetail.style.opacity = '1'
+    }, 400)
+    e.preventDefault()
+}
+jobDetail.children[0].onclick = function (e) {
+    jobDetail.style.opacity = '0';
+    
+    setTimeout(function () {
+        show( portfolioPage.children[1])
+        show( portfolioPage.children[2])
+        hide(jobDetail)
+       
+    }, 350)
+    setTimeout(function () {
+        portfolioPage.children[1].style.opacity = '1'
+        portfolioPage.children[2].style.opacity = '1'
+    }, 400)
+    e.preventDefault()
+}
 
